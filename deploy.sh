372dcd520275cbbf015b79f171df2c55280633f9
jiangshuangquan <954475209@qq.com>
2023-07-09 02:44:10 +0800

    feat: 测试提交

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jiangshuangquan/VitePressBlob.git master:gh-pages
+git push -f https://github.com/jiangshuangquan/VitePressBlob.git master:gh-pages
 
 cd -
